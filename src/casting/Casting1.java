4d1008141f00accb939760edd26a8dc601ec75a6
Seontaek Lim <seontaek1998@gmail.com>
2025-04-18 16:49:54 +0900

    scope

---
+++ src/casting/Casting1.java
@@ -0,0 +1,13 @@
+package casting;
+
+public class Casting1 {
+
+    public static void main(String[] args) {
+        int intValue = 20;
+        long longValue;
+        double doubleValue;
+
+        longValue = intValue;
+        System.out.println(longValue);
+    }
+}
---
+++ src/scope/Scope1.java
@@ -0,0 +1,18 @@
+package scope;
+
+public class Scope1 {
+
+    public static void main(String[] args) {
+        int m = 10;
+
+        if (true) {
+            int x = 20;
+            System.out.println(m);
+            System.out.println(x);
+        }
+
+        System.out.println(m);
+//        System.out.println(x);
+//        컴파일 에러
+    }
+}
