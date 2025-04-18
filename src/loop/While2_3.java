b0ea6cc96b5c895b3b95e5672d64f5280cb0102b
Seontaek Lim <seontaek1998@gmail.com>
2025-04-18 09:21:12 +0900

    while and doWhile

--- src/loop/While2_3.java
+++ src/loop/While2_3.java
@@ -5,7 +5,7 @@ public class While2_3 {
     public static void main(String[] args) {
         int sum = 0;
         int i = 1;
-        int endNum = 10;
+        int endNum = 3;
 
         while (i <= endNum) {
             sum = sum + i;
