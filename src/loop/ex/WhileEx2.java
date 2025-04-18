623756db90dc96b43784f527eaaa7d9854c0b5b7
Seontaek Lim <seontaek1998@gmail.com>
2025-04-18 15:33:47 +0900

    commit

---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,20 @@
+package loop.ex;
+
+public class WhileEx2 {
+
+    public static void main(String[] args) {
+        int num = 2;
+        int count = 1;
+
+//        while (count <= 10) {
+//            System.out.println(num * count);
+//            count++;
+//        }
+
+        while (count <= 10) {
+            System.out.println(num);
+            num += 2;
+            count++;
+        }
+    }
+}
