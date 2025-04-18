7ac9ff2bc58d2c25a2ed3869c78b762e053405a1
Seontaek Lim <seontaek1998@gmail.com>
2025-04-18 17:24:51 +0900

    casting

--- src/casting/Casting1.java
+++ src/casting/Casting1.java
@@ -3,11 +3,17 @@ package casting;
 public class Casting1 {
 
     public static void main(String[] args) {
-        int intValue = 20;
+        int intValue = 10;
         long longValue;
         double doubleValue;
 
-        longValue = intValue;
+        longValue = (long) intValue;
         System.out.println(longValue);
+
+        doubleValue = (double) intValue;
+        System.out.println(doubleValue);
+
+        doubleValue = 20L;
+        System.out.println(doubleValue);
     }
 }
