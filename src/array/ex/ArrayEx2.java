2f599e95f4297e6bc565dc900c481eca8bca0f34
Seontaek Lim <seontaek1998@gmail.com>
2025-04-23 17:00:11 +0900

    modify for enhance answer

--- src/array/ex/ArrayEx2.java
+++ src/array/ex/ArrayEx2.java
@@ -15,7 +15,7 @@ public class ArrayEx2 {
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i]);
-            if (i != numbers.length - 1) {
+            if (i < numbers.length - 1) {
                 System.out.print(", ");
             }
         }
