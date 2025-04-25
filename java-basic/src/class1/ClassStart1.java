6258f95d798b9a4b3af9210cc4cca2f2f66e509f
Seontaek Lim <seontaek1998@gmail.com>
2025-04-25 10:13:58 +0900

    java basic branch

--- java-basic/src/class1/ClassStart1.java
+++ java-basic/src/class1/ClassStart1.java
@@ -1,4 +1,14 @@
 package class1;
 
 public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String student2Name = "학생1";
+        int student2Age = 15;
+        int student2Grade = 90;
+    }
 }
