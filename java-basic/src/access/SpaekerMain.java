1e4cad03e33ed4ad804f76207917c06ef502544a
Seontaek Lim <seontaek1998@gmail.com>
2025-05-03 00:38:01 +0900

    class access

--- java-basic/src/access/SpaekerMain.java
+++ java-basic/src/access/SpaekerMain.java
@@ -18,7 +18,7 @@ public class SpaekerMain {
 
 //        필드에 직접 접근
         System.out.println("volume 필드 직접 접근 수정");
-        speaker.volume = 200;
+//        speaker.volume = 200;
         speaker.showVolume();
     }
 }
---
+++ java-basic/src/access/b/BankAccountMain.java
@@ -0,0 +1,9 @@
+package access.b;
+
+public class BankAccountMain {
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(10000);
+    }
+}
