e37c8adf0b9137de8646f97b4f7184c6ca2fee14
Seontaek Lim <seontaek1998@gmail.com>
2025-05-03 00:38:01 +0900

    class access

---
+++ java-basic/src/access/b/AccessOuterMain.java
@@ -0,0 +1,25 @@
+package access.b;
+
+import access.a.AccessData;
+
+public class AccessOuterMain {
+
+    public static void main(String[] args) {
+
+        AccessData data = new AccessData();
+
+//        public 호출 가능
+        data.publicField = 1;
+        data.publicMethod();
+
+//        동일 패키지 dafault 불가능
+//        data.defaultField = 2;
+//        data.defaultMethod();
+
+//        private 호출 불가
+//        data.privateField = 3;
+//        data.privateMethod();
+
+        data.innerAccess();
+    }
+}
