99a878260c810e95eaf9eaf1760247bd6ca1957d
Seontaek Lim <seontaek1998@gmail.com>
2025-06-20 16:35:27 +0900

    inner class

--- java-mid1/src/lang/immutable/test/ImmutableMyDateMain.java
+++ java-mid1/src/lang/immutable/test/ImmutableMyDateMain.java
@@ -12,7 +12,7 @@ public class ImmutableMyDateMain {
 
         System.out.println("2025 -> date1");
         // date1.setYear(2025);
-        date1 = date1.withYear(2025);
+        // date1 = date1.Year(2025);
         System.out.println("date1 = " + date1);
         System.out.println("date2 = " + date2);
     }
---
+++ java-mid1/src/nested/nested/NastedOuterMain.java
@@ -0,0 +1,12 @@
+package nested.nested;
+
+public class NastedOuterMain {
+
+    public static void main(String[] args) {
+        NestedOuter outer = new NestedOuter();
+        NestedOuter.Nested nested = new NestedOuter.Nested();
+        nested.print();
+
+        System.out.println("nestedClass = " + nested.getClass());
+    }
+}
