fdbe5b40377e6f40e6e94c8ce5d9d4b9d2d18cf5
Limselect <seontaek1998@gmail.com>
2025-07-06 18:38:00 +0900

    exception

--- java-mid1/src/exception/ex3/MainV3.java
+++ java-mid1/src/exception/ex3/MainV3.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
 
 public class MainV3 {
 
-    public static void main(String[] args) throws NetworkClientExceptionV3 {
-        NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+    public static void main(String[] args) {
+//        NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+        NetworkServiceV3_2 networkService = new NetworkServiceV3_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
--- java-mid1/src/exception/ex3/NetworkServiceV3_2.java
+++ java-mid1/src/exception/ex3/NetworkServiceV3_2.java
@@ -3,7 +3,6 @@ package exception.ex3;
 
 import exception.ex3.exception.ConnectExceptionV3;
 import exception.ex3.exception.NetworkClientExceptionV3;
-import exception.ex3.exception.SendExceptionV3;
 
 public class NetworkServiceV3_2 {
 
@@ -18,9 +17,11 @@ public class NetworkServiceV3_2 {
         } catch (ConnectExceptionV3 e) {
             System.out.println("[연결 오류] 주소: " + e.getAddress() + ", 메세지: " + e.getMessage());
         } catch (NetworkClientExceptionV3 e) {
-            System.out.println("[전송 오류] 전송 데이터: " + e.getSendData() + ", 메세지: " + e.getMessage());
+            System.out.println("[네트워크 오류] 메세지: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[알 수 없는 오류] 메세지: " + e.getMessage());
         } finally {
-            client.disconnect();
+                client.disconnect();
+            }
         }
-    }
 }
