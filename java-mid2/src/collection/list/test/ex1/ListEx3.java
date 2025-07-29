e8e44ecf7cad1875ae2cd9fb246676f787e1167c
Limselect <seontaek1998@gmail.com>
2025-07-29 14:48:39 +0900

    List test

--- java-mid2/src/collection/list/test/ex1/ListEx3.java
+++ java-mid2/src/collection/list/test/ex1/ListEx3.java
@@ -1,15 +1,16 @@
 package collection.list.test.ex1;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ListEx2 {
+public class ListEx3 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> numbers = new LinkedList<>();
+        List<Integer> numbers = new ArrayList<>();
 
+        int sum = 0;
         System.out.println("n개의 정수를 입력하세요 (종료 0)");
         while (true) {
             int input = sc.nextInt();
@@ -17,13 +18,10 @@ public class ListEx2 {
                 break;
             }
             numbers.add(input);
+            sum += input;
         }
-        System.out.println("출력");
-        for (int i = 0; i < numbers.size(); i++) {
-            System.out.print(numbers.get(i));
-            if (i < numbers.size() - 1) {
-                System.out.print(", ");
-            }
-        }
+        System.out.println("입력한 정수의 합계: " + sum);
+        double average = (double) sum / numbers.size();
+        System.out.println("입력한 정수의 평균: " +  average);
     }
 }
