b3dfa223d8d49827602dc0befca39e4f867cd488
Limselect <seontaek1998@gmail.com>
2025-08-16 12:45:12 +0900

    [MODIFY | Ignore modify]

---
+++ java-mid2/src/collection/set/StringHashMain.java
@@ -0,0 +1,23 @@
+package collection.set;
+
+public class StringHashMain {
+
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        // char
+        char charA = 'A';
+        char charB = 'B';
+        System.out.println("charA = " + (int)charA);
+        System.out.println("charB = " + (int)charB);
+
+//        int hashCode = hashCode("A");
+//        System.out.println("hashCode = " + hashCode);
+    }
+
+//    static int hashCode(String str) {
+//        char[] charArray = str.toCharArray();
+//        int sum = 0;
+//
+//    }
+}
