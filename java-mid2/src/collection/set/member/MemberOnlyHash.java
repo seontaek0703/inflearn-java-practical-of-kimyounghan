52108124897da94a14d646e5860d690108e10564
Seontaek Lim <seontaek1998@gmail.com>
2025-08-21 09:05:41 +0900

    mid2

--- java-mid2/src/collection/set/member/MemberOnlyHash.java
+++ java-mid2/src/collection/set/member/MemberOnlyHash.java
@@ -2,23 +2,16 @@ package collection.set.member;
 
 import java.util.Objects;
 
-public class Member {
+public class MemberOnlyHash {
 
     private String id;
 
-    public Member(String id) {
+    public MemberOnlyHash(String id) {
         this.id = id;
     }
 
     public String getId() {
         return id;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (object == null || getClass() != object.getClass()) return false;
-        Member member = (Member) object;
-        return Objects.equals(id, member.id);
     }
 
     @Override
@@ -28,7 +21,7 @@ public class Member {
 
     @Override
     public String toString() {
-        return "Member{" +
+        return "MemberOnlyHash{" +
                 "id='" + id + '\'' +
                 '}';
     }
